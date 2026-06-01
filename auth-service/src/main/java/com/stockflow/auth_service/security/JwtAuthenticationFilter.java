cac105ecb443818f26068ce6d59ed85852446605
AmarJeetSahi <amar.sahi70@gmil.com>
2026-06-01 18:26:11 +0530

    Added Jwt Filter chain

---
+++ auth-service/src/main/java/com/stockflow/auth_service/security/JwtAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package com.stockflow.auth_service.security;
+
+import com.stockflow.auth_service.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        System.out.println("JWT Filter Hit");
+
+
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+            String token =  authHeader.substring(7);
+
+            String email = jwtService.extractEmail(token);
+
+            if(jwtService.validateToken(token)){
+                System.out.println("User Email: "+email);
+
+            }
+        filterChain.doFilter(request, response);
+    }
+}
--- auth-service/src/main/java/com/stockflow/auth_service/service/JwtService.java
+++ auth-service/src/main/java/com/stockflow/auth_service/service/JwtService.java
@@ -2,8 +2,11 @@ package com.stockflow.auth_service.service;
 
 import com.stockflow.auth_service.dto.LoginRequestDto;
 import com.stockflow.auth_service.dto.UserDto;
+import com.stockflow.auth_service.security.JwtAuthenticationFilter;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,7 +17,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtService {
+
+
 
     @Value("${jwt.secret}")
     private String secretKey;
@@ -38,5 +45,34 @@ public class JwtService {
         return Keys.hmacShaKeyFor(
                 secretKey.getBytes(StandardCharsets.UTF_8)
         );
+    }
+
+    public String extractEmail(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public Date extractExpiration(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token)
+                .before(new Date());
+    }
+
+    public boolean validateToken(String token) {
+        return !isTokenExpired(token);
     }
 }
